2370cbb1c0c516c47f9561c2f137ed32db114fea
Miracle Anyanwu <o.amagwula@stu.unizik.edu.ng>
2021-10-21 06:59:39 +0100

    Setup static files, Templates and Navigation

--- pages/urls.py
+++ pages/urls.py
@@ -3,4 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.homepage, name='home'),
+    path('about', views.about, name='about'),
+    path('services', views.services, name='services'),
+    path('contact', views.contact, name='contact'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 # Create your views here.
 def homepage(request):
     return render(request, 'pages/homepage.html')
+
+def about(request):
+    return render(request, 'pages/about.html')
+
+def services(request):
+    return render(request, 'pages/services.html')
+
+def contact(request):
+    return render(request, 'pages/contact.html')
